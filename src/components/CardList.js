826da0e28fc8b4a4917605a317f110a44afe4316
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-04 09:57:33 +0100

    Fix unique key warning

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,8 +3,8 @@ import Card from './Card.js';
 
 const CardList = (props) => (
   <div id="container" className="mt-10">
-    {props.cards.map((card) => (
-      <Card {...card} />
+    {props.cards.map((card, index) => (
+      <Card key={index} {...card} />
     ))}
   </div>
 );
